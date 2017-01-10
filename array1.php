a690d8cb701c7aeea758267c6cbc2ef2622092ef
Ru Tan <jutan2012@gmail.com>
2017-01-10 11:17:38 +0100

    Function foreach ($array as $kay) Exer 1 done!

---
+++ array1.php
@@ -0,0 +1,18 @@
+<?php 
+$array = array( 
+'janvier', 
+'février',
+'mars',
+'avril',
+'mai',
+'juin',
+'juillet',
+'aout',
+'septembre',
+'octobre',
+'novembre',
+'décembre
+');
+foreach ($array as $key) {
+	echo "$key</br>";
+}
