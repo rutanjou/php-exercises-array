99b68e1bc1e6d76f370dc7695beb5497fb9a64bb
Ru Tan <jutan2012@gmail.com>
2017-01-10 13:52:00 +0100

    Exer10 done avec function foreach ($hautFrance as $key => $value)

---
+++ array10.php
@@ -0,0 +1,11 @@
+<?php  
+$hautFrance =array(
+	02=>"aisne",
+	59=>"nord",
+	60=>"oise",
+	62=>"pas-de-calais",
+	80=>"somme");
+foreach ($hautFrance as $key => $value){
+	# code...
+	echo "Le département $value a le numéro $key </br>";
+}
