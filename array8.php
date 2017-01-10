c5cf05ff9b7f3dd976c123207e94c74e08b19946
Ru Tan <jutan2012@gmail.com>
2017-01-10 13:30:14 +0100

    Exer8 done. function foreach ($hautFrance as $key)

---
+++ array8.php
@@ -0,0 +1,12 @@
+<?php  
+$hautFrance =array(
+	02=>"aisne",
+	59=>"nord",
+	60=>"oise",
+	62=>"pas-de-calais",
+	80=>"somme",
+	51=>"Reims");
+foreach ($hautFrance as $key){
+	# code...
+	echo "$key </br>";
+}
