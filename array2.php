3513d5d45941ef36d09894cda1540208d151f99e
Ru Tan <jutan2012@gmail.com>
2017-01-10 11:22:54 +0100

    Exer2 done avec function $array = array (3 => 'mars');

---
+++ array2.php
@@ -0,0 +1,16 @@
+<?php 
+$array = array( 
+'janvier', 
+'février',
+3 =>'mars',
+'avril',
+'mai',
+'juin',
+'juillet',
+'aout',
+'septembre',
+'octobre',
+'novembre',
+'décembre
+');
+echo $array[3];
